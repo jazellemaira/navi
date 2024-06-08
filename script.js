636f146cf47e15608280b71d872b1d66f2194c0b
jazellemaira <jazellemaira@pm.me>
2024-06-08 14:53:40 +0200

    Setting up map locations: current, success, error

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoibnN1bmFtaSIsImEiOiJjbHM5Y2dsNHkwMmlsMm5vNm16eDZmbGJlIn0.nh_8PWUFOjNvCcKrMQETcA"
 
-const map = new mapboxgl.Map({
-  accessToken: MAPBOX_ACCESS_TOKEN,
-  container: "map", // container ID
-  style: "mapbox://styles/mapbox/streets-v12", // style URL
-  center: [-74.5, 40], // starting position [lng, lat]
-  zoom: 9, // starting zoom
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
 })
+
+function setupMap(centerPosition) {
+  const map = new mapboxgl.Map({
+    accessToken: MAPBOX_ACCESS_TOKEN,
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v12",
+    center: centerPosition,
+    zoom: 15,
+  })
+}
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude])
+  console.log(position)
+}
+
+function errorLocation() {
+  setupMap([5.31, 51.68])
+}
