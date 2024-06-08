23a1e1dbf72e5ff0c6024b7a2ea7048e69d4e2e0
jazellemaira <jazellemaira@pm.me>
2024-06-08 14:32:46 +0200

    Making the access token into a constant variable

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-mapboxgl.accessToken =
+const MAPBOX_ACCESS_TOKEN =
   "pk.eyJ1IjoibnN1bmFtaSIsImEiOiJjbHM5Y2dsNHkwMmlsMm5vNm16eDZmbGJlIn0.nh_8PWUFOjNvCcKrMQETcA"
+
 const map = new mapboxgl.Map({
+  accessToken: MAPBOX_ACCESS_TOKEN,
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v12", // style URL
   center: [-74.5, 40], // starting position [lng, lat]
